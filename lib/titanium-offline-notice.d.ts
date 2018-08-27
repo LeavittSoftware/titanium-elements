da534d50f2daef443e1ab01a8c3820ae63f7b01c
Aaron Drabeck <github@aarondrabeck.com>
2018-08-27 16:26:13 -0600

    Removes js from source control and builds it on the build server.

--- lib/titanium-offline-notice.d.ts
+++
@@ -1,7 +0,0 @@
-import { PolymerElement } from '@polymer/polymer';
-export declare class TitaniumOfflineNoticeElement extends PolymerElement {
-    isOnline: boolean;
-    ready(): void;
-    onlineStatusChanged(): void;
-    static readonly template: HTMLTemplateElement;
-}
--- lib/titanium-sw-notifier.d.ts
+++
@@ -1,7 +0,0 @@
-import { PolymerElement } from '@polymer/polymer';
-export declare class TitanuimServiceWorkerNotifierElement extends PolymerElement {
-    notificationsStatus: string;
-    ready(): Promise<void>;
-    _reload(): void;
-    static readonly template: HTMLTemplateElement;
-}
