8b60ae991384e6d2536e20cdfddf2b05b3b3626c
Unknown <anthony-thomas@leavitt.com>
2018-09-18 11:14:07 -0600

    Moves StringMap and AnyFunction into titanium-types. Renames action-helpers

--- src/titanium-redux-action-helpers.ts
+++ src/titanium-redux-action-helpers.ts
@@ -1,16 +1,6 @@
-/**
- * use this type definition instead of `Function` type constructor
- */
-export type AnyFunction = (...args: any[]) => any;
+import { StringMap, AnyFunction } from './titanium-types';
 
-/**
- * simple alias to save you keystrokes when defining JS typed object maps
- */
-export type StringMap<T> = {
-  [key: string]: T
-};
-
-export type Action<T extends string = string, P = void> = P extends void ? Readonly<{type: T}>: Readonly<{type: T; payload: P}>;
+export type Action<T extends string = string, P = void> = P extends void ? Readonly<{ type: T }> : Readonly<{ type: T; payload: P }>;
 
 export type ActionsUnion<A extends StringMap<AnyFunction>> = ReturnType<A[keyof A]>;
 
