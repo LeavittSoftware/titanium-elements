a8ea023253c9f094cb961120f37646eb7ad88f0e
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-10-08 15:11:50 -0600

    Removes paper-ripple. Adds disabled. Formats label better. Adds more demos of chips

--- src/titanium-chip.ts
+++ src/titanium-chip.ts
@@ -1,6 +1,5 @@
 import '@vaadin/vaadin-button/theme/material/vaadin-button';
 import '@polymer/iron-flex-layout/iron-flex-layout';
-import '@polymer/paper-ripple/paper-ripple';
 
 import {customElement, property} from '@polymer/decorators';
 import {html, PolymerElement} from '@polymer/polymer';
@@ -10,6 +9,13 @@ import {html, PolymerElement} from '@polymer/polymer';
 export class TitaniumChipElement extends PolymerElement {
   @property() label: string;
   @property() src: string|null = null;
+  @property({type: Boolean, reflectToAttribute: true})
+  disabled: boolean = false;
+
+  _onCloseButtonClicked() {
+    this.dispatchEvent(new CustomEvent(
+        'titanium-chip-close', {bubbles: true, composed: true}));
+  }
 
   static get template() {
     return html`<style>
@@ -21,8 +27,16 @@ export class TitaniumChipElement extends PolymerElement {
     border-radius: 16px;
     cursor: pointer;
     user-select: none;
-    min-height: 28px;
+    height: 30px;
     @apply --titanium-chip;
+  }
+
+  :host([disabled]) {
+    opacity: .6;
+  }
+
+  :host([disabled]) {
+    background-color: #eee !important;
   }
 
   :host(:hover) {
@@ -31,28 +45,40 @@ export class TitaniumChipElement extends PolymerElement {
     @apply --titanium-chip-hover;
   }
 
+  :host(:active) {
+    background-color: #ccc;
+    @apply --titanium-chip-active;
+  }
+
+  :host ::slotted(*) {
+    margin-left: 8px;
+  }
+
   label {
     @apply --layout-flex-auto;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
     -webkit-font-smoothing: antialiased;
-    padding: 0 8px;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 20px;
+    margin: 0 16px;
+    font-size: 13px;
+    font-weight: 500;
+    line-height: 20px;    
+    color: #424242;
     cursor: pointer;
     @apply --titanium-chip-label;
   }
 
   img {
     border-radius: 50%;
+    border: 1px solid transparent;
     height: 28px;
     width: 28px;
     @apply --titanium-chip-image;
   }
 
   vaadin-button {
+    display: none;
     margin: 0;
     padding: 0;
     height: 28px;
@@ -60,8 +86,13 @@ export class TitaniumChipElement extends PolymerElement {
     min-width: 0;
     border-radius: 50%;
     flex-shrink: 0;
-    display: none;
+    color: #424242;
     @apply --titanium-chip-button;
+  }
+
+  vaadin-button::after,
+  vaadin-button::before {
+    opacity: 0;
   }
 
   :host([closeable]) vaadin-button {
@@ -76,12 +107,11 @@ export class TitaniumChipElement extends PolymerElement {
   }
 </style>
 
-<paper-ripple></paper-ripple>
 <slot slot="chip-icon" name="chip-icon">
   <img src="[[src]]" hidden$="[[!src]]" />
 </slot>
 <label>[[label]]</label>
-<vaadin-button>
+<vaadin-button disabled$="[[disabled]]" on-click="_onCloseButtonClicked">
   <svg prefix viewBox="0 0 24 24">
     <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
   </svg>
