c44d9b3f8ab2c5691eb221192be305a657665f22
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-10-09 17:01:14 -0600

    Removes comment. Tells it to look for ts instead of js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import {configure} from '@storybook/polymer';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../stories', true, /.stories.ts$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
