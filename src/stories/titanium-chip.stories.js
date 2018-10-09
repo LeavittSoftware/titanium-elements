ca731c58427264b5aee8c80747f259e9b4e19471
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-10-09 13:02:15 -0600

    Adds titanium chip stories

---
+++ src/stories/titanium-chip.stories.js
@@ -0,0 +1,15 @@
+// TODO: add demo stories
+import '../../lib/titanium-chip';
+
+import {storiesOf} from '@storybook/polymer';
+
+storiesOf('Titanium Chip', module)
+    .add('basic', () => `<titanium-chip label="James Franco"></titanium-chip>`)
+    .add(
+        'with profile picture',
+        () =>
+            `<titanium-chip label="James Franco" src="https://mapi.leavitt.com/People(771130)/Default.Picture(size=35)"></titanium-chip>`)
+    .add(
+        'closeable',
+        () =>
+            `<titanium-chip label="James Franco" src="https://mapi.leavitt.com/People(771130)/Default.Picture(size=35)" closeable></titanium-chip>`);
