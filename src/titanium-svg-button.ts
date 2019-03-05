f424bd5890648ba69a93aae90ec0cba0ed07e209
Aaron Drabeck <github@aarondrabeck.com>
2019-03-04 17:21:31 -0700

    Adds WIP svg-button and demos.

---
+++ src/titanium-svg-button.ts
@@ -0,0 +1,54 @@
+import '@material/mwc-ripple';
+import {css, customElement, html, LitElement, property} from 'lit-element';
+
+@customElement('titanium-svg-button')
+export class TitaniumSvgButton extends LitElement {
+  @property() path: string;
+  @property({type: Boolean, reflect: true}) disabled: boolean;
+
+  static styles = css`
+   :host {
+     display: block;
+     cursor: pointer;
+     width: 42px;
+     height: 42px;
+
+     @apply --titanium-svg-button;
+   }
+  
+   :host([large]) {
+    width: 64px;
+    height: 64px;
+   }
+
+   ripple-contianer { 
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+   }
+   
+   svg {
+    margin: 0 auto;
+    display: block;
+    height: 65%;
+    width: 65%;
+    fill: var(--titanium-svg-button-svg-active-color, #3b95ff);
+    @apply --titanium-svg-button-active-color;
+   }
+   
+   :host([disabled]) svg {
+    fill: var(--titanium-svg-button-svg-disabled-color, #9b9b9b);
+   }`;
+
+  // Render element DOM by returning a `lit-html` template.
+  render() {
+    return html`<ripple-contianer>
+       <svg viewBox="0 0 24 24">
+        <path d="${this.path}" />
+       </svg>
+       <mwc-ripple ?disabled=${this.disabled} unbounded></mwc-ripple>
+     </ripple-contianer>`;
+  }
+}
