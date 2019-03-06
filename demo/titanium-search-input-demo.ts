c0d9a4da347bd18d23d986102e8ce9b5cd8918f1
Kasey <kaseyjameshinton@gmail.com>
2019-03-06 11:29:58 -0700

    Makes a demo for search to hook up 2 way binding.

---
+++ demo/titanium-search-input-demo.ts
@@ -0,0 +1,28 @@
+import '@leavittsoftware/titanium-elements/lib/titanium-search-input';
+
+import {customElement, observe, property} from '@polymer/decorators';
+import {html, PolymerElement} from '@polymer/polymer/polymer-element';
+
+@customElement('titanium-search-input-demo')
+export default class TitaniumSearchInputDemo extends PolymerElement {
+  @property() searchTerm: string;
+
+  static get template() {
+    return html`<style>
+    h1 {
+        @apply --paper-font-title;
+    }
+
+    search-term {
+        display: block;
+        @apply --paper-font-body2;
+        padding-bottom: 16px;
+    }
+</style>
+<div>
+    <h3>Titanium Search Input</h3>
+    <search-term>Term: [[searchTerm]]</search-term>
+    <titanium-search-input placeholder="Search" value="{{searchTerm}}"></titanium-search-input>
+</div>`;
+  }
+}
