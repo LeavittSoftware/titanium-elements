9445ab75b4591dba419d5a5ed52f8e6a8825bf3d
Aaron Drabeck <github@aarondrabeck.com>
2019-03-06 12:01:59 -0700

    Updates story book for header demo.

--- stories/titanium-table-header-stories.ts
+++ stories/titanium-table-header-stories.ts
@@ -1,6 +1,6 @@
 
 
-import '../demo/titanium-table-header-demo';
+import '../lib/titanium-table-header';
 
 import {withNotes} from '@storybook/addon-notes';
 import {storiesOf} from '@storybook/polymer/dist/client/preview/index';
@@ -14,8 +14,12 @@ const basic = `
 </header>
 `;
 
-const demo = `<titanium-table-header-demo></titanium-table-header-demo>`;
+const noSort = `
+<header style="border-bottom: 1px solid #dadce0;border-top: 1px solid #dadce0;">
+<titanium-table-header no-sort column-name="name" title="Name"></titanium-table-header>
+</header>
+`;
 
 storiesOf('Table Header', module)
     .add('basic', withNotes(notes)(() => basic))
-    .add('demo', withNotes(notes)(() => demo));
+    .add('no sort', withNotes(notes)(() => noSort));
