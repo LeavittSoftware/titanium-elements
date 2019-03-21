d94316e2d3ef603236297a06ff783bbe7d8bccd2
Daniel Nielson <daniel@unfairsurprisery.org>
2019-03-21 15:52:19 -0600

    Handle clear

--- src/titanium-company-selector.ts
+++ src/titanium-company-selector.ts
@@ -253,7 +253,8 @@ export class TitaniumCompanySelectorElement extends LitElement {
       slot="suffix"
       ?hidden="${!this.isLoading}">
       <div></div><div></div><div></div><div></div></lds-ring>
-    <svg slot="suffix" class="clear-button" viewBox="0 0 24 24">
+    <svg slot="suffix" class="clear-button" @click="${
+        this.clear}" viewBox="0 0 24 24">
       <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
     </svg>
   </vaadin-text-field>
