639967896be8b94449846fcb1d729a660d51b21a
Aaron Drabeck <github@aarondrabeck.com>
2019-04-23 12:04:43 -0600

    Moves stories.

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -22,7 +22,7 @@ const litDecorator = makeDecorator({
 addDecorator(litDecorator);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/stories', true, /\.stories\.js$/);
+const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
