6222129870ca3a38159e78709173439c316a781a
Aaron Drabeck <github@aarondrabeck.com>
2019-05-28 15:03:02 -0600

    feat: Adds new titanium-card elements.

---
+++ packages/titanium-card/src/titanium-card.ts
@@ -0,0 +1,87 @@
+﻿import { css, customElement, html, LitElement } from 'lit-element';
+
+export const TitaniumCardStyles = css`
+  ::slotted(h1),
+  h1 {
+    display: block;
+    color: var(--app-darker-text-color, #202124);
+
+    font-family: Roboto, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    letter-spacing: -0.264px;
+    font-weight: 400;
+    font-size: 22px;
+    line-height: 28px;
+    margin: 0;
+    padding: 24px 24px 12px 24px;
+  }
+
+  ::slotted(h2),
+  h2 {
+    display: block;
+    color: #000000de;
+
+    font-family: Roboto, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    letter-spacing: -0.264px;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    margin: 0;
+    padding: 24px 24px 12px 24px;
+  }
+
+  ::slotted(h3),
+  h3 {
+    display: block;
+    color: #000000de;
+
+    font-family: Roboto, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    letter-spacing: -0.264px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    margin: 0;
+    padding: 24px 24px 12px 24px;
+  }
+
+  ::slotted(p),
+  p {
+    font-family: Roboto, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-size: 14px;
+    font-weight: 400;
+    color: var(--app-text-color, #5f6368);
+    margin: 0;
+    padding: 0 24px 24px;
+    letter-spacing: 0.2px;
+    line-height: 20px;
+  }
+`;
+
+@customElement('titanium-card')
+export class TitaniumCardElement extends LitElement {
+  static styles = css`
+    ${TitaniumCardStyles}
+    :host {
+      display: flex;
+      flex-direction: column;
+
+      border: 1px solid var(--app-border-color, #dadce0);
+      border-radius: 8px;
+
+      overflow: hidden;
+    }
+  `;
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
---
+++ packages/titanium-card/src/titanium-single-action-card.ts
@@ -0,0 +1,72 @@
+﻿import { customElement, html, property, css, LitElement } from 'lit-element';
+import { TitaniumCardElement } from './titanium-card';
+import '@material/mwc-ripple';
+
+@customElement('titanium-single-action-card')
+export class TitaniumSingleActionCardElement extends LitElement {
+  @property({ type: String }) buttonTitle;
+
+  static styles = css`
+    ${TitaniumCardElement.styles}
+
+    :host {
+      display: flex;
+      flex-direction: column;
+
+      border: 1px solid var(--app-border-color, #dadce0);
+      border-radius: 8px;
+
+      font-family: Roboto, sans-serif;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+
+      overflow: hidden;
+    }
+
+    a {
+      display: block;
+      border-top: 1px solid var(--app-border-color, #dadce0);
+      color: var(--app-link-color, #1a73e8);
+      cursor: pointer;
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 20px;
+      padding: 17px 24px 18px 24px;
+      letter-spacing: 0.2px;
+      -webkit-user-select: none; /* Safari 3.1+ */
+      -moz-user-select: none; /* Firefox 2+ */
+      -ms-user-select: none; /* IE 10+ */
+      user-select: none; /* Standard syntax */
+      text-decoration: none;
+
+      border-radius: 0 0 8px 8px;
+    }
+
+    :host([disabled]) a {
+      pointer-events: none;
+      color: rgba(0, 0, 0, 0.37);
+    }
+
+    a:hover {
+      background-color: var(--app-hover-color, #f9f9f9);
+      transition: 0.3s ease;
+    }
+  `;
+
+  render() {
+    return html`
+      <slot></slot>
+      <a
+        title=${this.buttonTitle}
+        href=${this.buttonTitle}
+        @click=${(e: Event) => {
+          e.preventDefault();
+          this.dispatchEvent(new Event('action-click'));
+        }}
+      >
+        ${this.buttonTitle}
+        <mwc-ripple></mwc-ripple>
+      </a>
+    `;
+  }
+}
