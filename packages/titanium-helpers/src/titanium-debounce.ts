b058906b17937fa3898a54f0652c411417775eb2
Aaron Drabeck <aaron-drabeck@leavitt.com>
2019-12-13 13:37:27 -0700

    feat(titanium-helpers): Adds Debounce class; deprecates debouce function.
    Adds typing to debounce function
    Adds new promise driven Dounce class

--- packages/titanium-helpers/src/titanium-debounce.ts
+++ packages/titanium-helpers/src/titanium-debounce.ts
@@ -1,18 +1,53 @@
-import { AnyFunction } from './titanium-types';
-
-// tslint:disable: no-any
-export const debounce = (callBack: AnyFunction, interval: number) => {
+/**
+ * @deprecated since version 2.1.2
+ */
+export function debounce<T, A>(callBack: (...args: A[]) => Promise<T>, interval: number) {
   let timer: number;
-  let previousPromise: Promise<any> = Promise.resolve();
-  return (...args): Promise<any> => {
+  let previousPromise: Promise<T>;
+  return (...args: A[]) => {
     clearTimeout(timer);
-    const tempPromise = new Promise(function(resolve) {
+    const deferred = new Promise<T>(resolve => {
       timer = window.setTimeout(async () => {
         await previousPromise;
-        previousPromise = tempPromise;
+        previousPromise = deferred;
         resolve(callBack(...args));
       }, interval);
     });
-    return tempPromise;
+    return deferred;
   };
-};
+}
+
+export class Debounce<T, A> {
+  private _debouncePromise: Promise<T> | undefined;
+  private _debounceResolve: { (arg0: T): void; (value?: T | PromiseLike<T> | undefined): void };
+  private timer: number = 0;
+  private work: (...args: A[]) => Promise<T>;
+  private interval: number;
+
+  constructor(promise: (...args: A[]) => Promise<T>, interval: number = 300) {
+    this.work = promise;
+    this.interval = interval;
+  }
+
+  /**
+   * Returns the same pending promise each time called until interval expires.
+   *
+   * When the interval expires, the work promise is executed with the args from the latest call
+   * to debounce. Then all pending promises are resolved with the value from the work promise.
+   *
+   */
+  debounce(...args: A[]) {
+    clearTimeout(this.timer);
+    this.timer = window.setTimeout(async () => {
+      this._debounceResolve(await this.work(...args));
+      this._debouncePromise = undefined;
+    }, this.interval);
+
+    if (!this._debouncePromise) {
+      this._debouncePromise = new Promise<T>(async res => {
+        this._debounceResolve = res;
+      });
+    }
+    return this._debouncePromise;
+  }
+}
