e5007dddda93ff8915f43d94f094c57e3deab9dc
Aaron Drabeck <github@aarondrabeck.com>
2020-04-01 15:40:53 -0600

    Refactor: workbox complient
    BREAKING: Must use with google workbox

--- packages/titanium-sw-notifier/src/titanium-sw-notifier.ts
+++ packages/titanium-sw-notifier/src/titanium-sw-notifier.ts
@@ -6,59 +6,41 @@ export class TitanuimServiceWorkerNotifierElement extends LitElement {
   @property({ type: String }) notificationsStatus: string;
   @property({ type: String }) scriptUrl: string = 'service-worker.js';
 
-  async firstUpdated() {
-    if ('serviceWorker' in navigator) {
-      const registration = await navigator.serviceWorker.register(this.scriptUrl, {
-        scope: '/',
-      });
+  private _newWorker: ServiceWorker | null;
+  private _refreshing = false;
 
-      registration.addEventListener('updatefound', () => {
-        const installingWorker = registration.installing;
-        if (!installingWorker) {
+  async connectedCallback() {
+    if ('serviceWorker' in navigator) {
+      const reg = await navigator.serviceWorker.getRegistration();
+      if (reg) {
+        reg.addEventListener('updatefound', () => {
+          this._newWorker = reg.installing;
+          this._newWorker?.addEventListener('statechange', () => {
+            if (this._newWorker?.state === 'installed' && navigator.serviceWorker.controller) {
+              this._showUpdatedSnackbar();
+            }
+          });
+        });
+
+        if (reg.waiting && navigator.serviceWorker.controller) {
+          this._newWorker = reg.waiting;
+          this._showUpdatedSnackbar();
+        }
+      }
+
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        if (this._refreshing) {
           return;
         }
-
-        installingWorker.addEventListener('statechange', async () => {
-          if (installingWorker.state === 'installed' && !navigator.serviceWorker.controller) {
-            this._showInstalledSnackbar();
-          }
-        });
+        window.location.reload();
+        this._refreshing = true;
       });
-
-      if ('PushManager' in window && 'Notification' in window) {
-        registration.pushManager.getSubscription().then(subscription => {
-          this.notificationsStatus = subscription ? 'subscribed' : 'unsubscribed';
-          this.dispatchEvent(new CustomEvent('notifications-status-changed', { detail: this.notificationsStatus }));
-        });
-      }
-
-      // Check to see if the service worker controlling the page at initial load
-      // has become redundant, since this implies there's a new service worker
-      // with fresh content.
-      if (navigator.serviceWorker.controller) {
-        // tslint:disable-next-line: no-any
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        navigator.serviceWorker.controller.addEventListener('statechange', (event: any) => {
-          if (event && event.target && event.target.state === 'redundant') {
-            this._showUpdatedSnackbar();
-          }
-        });
-      }
     }
   }
 
   private async _showUpdatedSnackbar() {
     await AppSnackbar.open('Site has been updated', { actionText: 'RELOAD' });
-    window.location.reload();
-  }
-
-  private async _showInstalledSnackbar() {
-    const closeTimeout = setTimeout(() => {
-      AppSnackbar.close();
-    }, 5000);
-
-    await AppSnackbar.open('Site cached for offline use', { actionText: 'DISMISS' });
-    clearTimeout(closeTimeout);
+    this._newWorker?.postMessage({ type: 'SKIP_WAITING' });
   }
 
   render() {
