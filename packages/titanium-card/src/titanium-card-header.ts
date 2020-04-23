d0d2e05714a51b6bce5abfd4277316e364b54008
Aaron Drabeck <github@aarondrabeck.com>
2020-04-23 11:14:22 -0600

    feat: card header brings its own content slot styles

--- packages/titanium-card/src/titanium-card-header.ts
+++ packages/titanium-card/src/titanium-card-header.ts
@@ -73,6 +73,49 @@ export class TitaniumCardHeaderElement extends LitElement {
     div[content] {
       grid-area: content;
     }
+
+    div[content] ::slotted(h1) {
+      display: block;
+      color: var(--app-dark-text-color, #202124);
+
+      font-family: Metropolis, 'Roboto', 'Noto', sans-serif;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      letter-spacing: -0.264px;
+      font-weight: 400;
+      font-size: 22px;
+      line-height: 28px;
+      margin: 0;
+      padding: 24px 24px 12px 24px;
+    }
+
+    div[content] ::slotted(h2) {
+      display: block;
+      color: #000000de;
+
+      font-family: Roboto, sans-serif;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      letter-spacing: -0.264px;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 24px;
+      margin: 0;
+      padding: 24px 24px 12px 24px;
+    }
+
+    div[content] ::slotted(p) {
+      font-family: Roboto, sans-serif;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      font-size: 14px;
+      font-weight: 400;
+      color: var(--app-text-color, #5f6368);
+      margin: 0;
+      padding: 0 24px 24px;
+      letter-spacing: 0.2px;
+      line-height: 20px;
+    }
   `;
 
   render() {
