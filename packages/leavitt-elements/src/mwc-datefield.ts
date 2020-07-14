bcb8f5b7a4418e2c3d92675f7ead4fc4e8afc385
Aaron Drabeck <aaron-drabeck@leavitt.com>
2020-07-14 11:30:29 -0600

    build: ^ wca

---
+++ packages/leavitt-elements/src/mwc-datefield.ts
@@ -0,0 +1,28 @@
+import { customElement, css } from 'lit-element';
+import { TextField } from '@material/mwc-textfield';
+
+@customElement('mwc-datefield')
+export class DateField extends TextField {
+  constructor() {
+    super();
+    this.type = 'date';
+    this.iconTrailing = 'event';
+    this.placeholder = 'yyyy-mm-dd';
+    this.outlined = true;
+  }
+
+  static get styles() {
+    return css`
+      ${super.styles}
+      input::-webkit-calendar-picker-indicator {
+        position: absolute;
+        top: 0;
+        right: 0;
+        opacity: 0;
+        width: 48px;
+        height: 100%;
+        cursor: pointer;
+      }
+    `;
+  }
+}
