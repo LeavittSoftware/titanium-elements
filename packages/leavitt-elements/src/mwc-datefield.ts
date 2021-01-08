9af49b76a31f2a3db12600385e42f1b59f929a83
Aaron Drabeck <aaron-drabeck@leavitt.com>
2021-01-08 14:14:27 -0700

    feat: allow date field to accept date and datetime local

--- packages/leavitt-elements/src/mwc-datefield.ts
+++ packages/leavitt-elements/src/mwc-datefield.ts
@@ -1,13 +1,21 @@
-import { customElement, css } from 'lit-element';
+import { customElement, css, property } from 'lit-element';
 import { TextField } from '@material/mwc-textfield';
 
 @customElement('mwc-datefield')
 export class DateField extends TextField {
+  @property({ type: String, attribute: 'date-type' }) dateType: 'datetime-local' | 'date' = 'date';
+
+  updated(changedProps: Map<keyof this, unknown>) {
+    if (changedProps.has('dateType')) {
+      console.log(this.dateType);
+      this.placeholder = this.dateType === 'date' ? 'yyyy-mm-dd' : 'yyyy-mm-dd MM:hh:ss';
+      this.type = this.dateType;
+    }
+  }
   constructor() {
     super();
     this.type = 'date';
     this.iconTrailing = 'event';
-    this.placeholder = 'yyyy-mm-dd';
     this.outlined = true;
   }
 
