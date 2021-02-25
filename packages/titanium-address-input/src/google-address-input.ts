a4dd8f85f5dc35ae5ba61cf07620527f3edbb660
Daniel Nielson <daniel@unfairsurprisery.org>
2021-02-25 15:15:03 -0700

    fix: missing geometry field

--- packages/titanium-address-input/src/google-address-input.ts
+++ packages/titanium-address-input/src/google-address-input.ts
@@ -98,7 +98,7 @@ export class GoogleAddressInput extends LitElement {
 
   private async _setUpAutocomplete() {
     await this.input.updateComplete;
-    this.autocomplete = new google.maps.places.Autocomplete(this.input.formElement, { fields: ['address_components', 'formatted_address'], types: ['address'] });
+    this.autocomplete = new google.maps.places.Autocomplete(this.input.formElement, { fields: ['address_components', 'formatted_address', 'geometry'], types: ['address'] });
     google.maps.event.addListener(this.autocomplete, 'place_changed', this._onPlaceChanged.bind(this));
   }
 
