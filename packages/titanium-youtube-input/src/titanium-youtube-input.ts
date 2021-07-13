b506ce69d652fecadbde03e57170eb2ee87ec6bc
Aaron Drabeck <aaron-drabeck@leavitt.com>
2021-07-13 09:50:59 -0600

    refactor: add documentation; rename key prop

--- packages/titanium-youtube-input/src/titanium-youtube-input.ts
+++ packages/titanium-youtube-input/src/titanium-youtube-input.ts
@@ -1,26 +1,73 @@
 import { LitElement, css, html, customElement, property, query } from 'lit-element';
 import '@material/mwc-textfield';
 import { TextField } from '@material/mwc-textfield';
+/**
+ * Material design YouTube url input.
+ *
+ * @element titanium-youtube-input
+ *
+ * @fires video-changed - Fired when the YouTube video key changes
+ *
+ */
 
 @customElement('titanium-youtube-input')
-export class TitaniumYoutubeInputElement extends LitElement {
-  @property({ type: String }) youtubeVideoKey: string | null | undefined;
-  @property({ type: Boolean }) required: boolean;
-  @property({ type: Boolean }) disabled: boolean;
-  @property({ type: String }) validationMessage: string;
-  @property({ type: String }) label: string;
-  @query('mwc-textfield') input: TextField & { mdcFoundation: { setValid(): boolean }; isUiValid: boolean };
+export class TitaniumYouTubeInputElement extends LitElement {
+  /**
+   * Unique YouTube video key
+   */
+  @property({ type: String, attribute: 'youtube-video-key' }) youTubeVideoKey: string | null | undefined;
 
-  public reportValidity() {
-    return this.input.reportValidity() && this.youtubeVideoKey;
+  /**
+   *  Displays error state if value is empty and input is blurred.
+   */
+  @property({ type: Boolean }) required: boolean;
+
+  /**
+   * Disables the input
+   */
+  @property({ type: Boolean }) disabled: boolean;
+
+  /**
+   *  	Message to show in the error color when the textfield is invalid. (Helper text will not be visible)
+   */
+  @property({ type: String }) validationMessage: string;
+
+  /**
+   *  Sets floating label value.
+   */
+  @property({ type: String }) label: string = 'YouTube Video Key';
+
+  @query('mwc-textfield') private input!: TextField & { mdcFoundation: { setValid(): boolean }; isUiValid: boolean };
+
+  firstUpdated() {
+    this.input.validityTransform = (newValue, nativeValidity) => {
+      if (!nativeValidity.valid) {
+        return {};
+      } else {
+        const len = newValue?.length ?? 0;
+        const isValid = len > 6 && len < 12;
+        return {
+          valid: isValid,
+          customError: !isValid,
+        };
+      }
+    };
   }
 
-  public reset() {
+  reportValidity() {
+    return this.input.reportValidity();
+  }
+
+  checkValidity() {
+    return this.input.checkValidity();
+  }
+
+  reset() {
     this.input.isUiValid = true;
     this.input.mdcFoundation?.setValid?.(true);
   }
 
-  private _getYoutubeKey(value: string) {
+  private _getYouTubeKey(value: string) {
     // https://stackoverflow.com/a/8260383
     const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     const match = value.match(regExp);
@@ -31,22 +78,23 @@ export class TitaniumYoutubeInputElement extends LitElement {
     return key ? key : value;
   }
 
-  private async clearYoutubeInputValue() {
-    this.youtubeVideoKey = '';
+  private async _clearYouTubeInputValue() {
+    this.youTubeVideoKey = '';
     return this.updateComplete;
   }
-  private async _youtubeKeyChanged(event) {
+
+  private async _youTubeKeyChanged(event) {
     if (event && event.target && event.target.value) {
       const value = event.target.value;
       // Clears the input before resetting it with just the key
-      await this.clearYoutubeInputValue();
-      this.youtubeVideoKey = this._getYoutubeKey(value);
+      await this._clearYouTubeInputValue();
+      this.youTubeVideoKey = this._getYouTubeKey(value);
     } else {
-      this.youtubeVideoKey = '';
+      this.youTubeVideoKey = '';
     }
 
     // values changed
-    this.dispatchEvent(new CustomEvent('video-changed', { detail: { key: this.youtubeVideoKey } }));
+    this.dispatchEvent(new CustomEvent('video-changed', { detail: { key: this.youTubeVideoKey } }));
   }
 
   static styles = css`
@@ -74,12 +122,12 @@ export class TitaniumYoutubeInputElement extends LitElement {
         ?disabled=${this.disabled}
         .validationMessage=${this.validationMessage}
         .label=${this.label}
-        @input="${this._youtubeKeyChanged}"
-        .value=${this.youtubeVideoKey || ''}
+        @input="${this._youTubeKeyChanged}"
+        .value=${this.youTubeVideoKey || ''}
       ></mwc-textfield>
-      ${this.youtubeVideoKey &&
+      ${this.youTubeVideoKey &&
       html`
-        <img title="video thumbnail" src="https://img.youtube.com/vi/${this.youtubeVideoKey.length === 11 ? this.youtubeVideoKey + '/' : ''}default.jpg" />
+        <img title="video thumbnail" src="https://img.youtube.com/vi/${this.youTubeVideoKey.length === 11 ? this.youTubeVideoKey + '/' : ''}default.jpg" />
       `}
     `;
   }
