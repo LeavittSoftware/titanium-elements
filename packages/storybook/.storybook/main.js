b2b384757559403b4ad2c2f5b3871639482f5b47
Kasey <kaseyjameshinton@gmail.com>
2021-10-04 16:58:26 -0600

    doc: set up titanium-button story appropriately so we can use controls/actions

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
+  logLevel: 'debug',
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '@storybook/addon-storysource',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-storysource', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack4',
+  },
 };
 
 /** @storybook/addon-essentials Included
--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,20 +1,24 @@
-import customElementsManifest from '../custom-elements.json';
-
 import { setCustomElements } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
 
 setCustomElements(customElements);
 
-// window.__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__ = customElementsManifest;
-
 export const parameters = {
-    passArgsFirst: false,
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+  controls: {
+    expanded: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-  };
-
+  },
+  a11y: {
+    config: {},
+    options: {
+      checks: { 'color-contrast': { options: { noScroll: true } } },
+      restoreScroll: true,
+    },
+  },
+  docs: {
+    iframeHeight: '200px',
+  },
+};
