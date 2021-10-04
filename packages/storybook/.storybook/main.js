ea4761539ad0393a1b1e89557f8f4e3b168146db
Kasey <kaseyjameshinton@gmail.com>
2021-10-04 11:41:55 -0600

    feat: storybook 6.3 upgrade. WIP

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-docs/preset', '@storybook/addon-knobs/preset', '@storybook/addon-links', '@storybook/addon-essentials'],
+};
---
+++ packages/storybook/.storybook/preview.js
@@ -0,0 +1,3 @@
+export const parameters = {
+    passArgsFirst: false,
+  };
