652c3a8d8345c714a4ecd67786e929ffd72a06d1
Kasey <kaseyjameshinton@gmail.com>
2021-10-04 16:19:59 -0600

    feat: Massive storybook upgrade.
    Migrates to newest version for many things. web component analyzer now generates an experimental version of custom-elements.json required for lit 2 and storybook 6.3

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,4 +1,18 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-docs/preset', '@storybook/addon-knobs/preset', '@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    '@storybook/addon-essentials',
+  ],
 };
+
+/** @storybook/addon-essentials Included
+Actions
+Backgrounds
+Controls
+Docs
+Viewport
+Toolbars
+**/
--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,3 +1,20 @@
+import customElementsManifest from '../custom-elements.json';
+
+import { setCustomElements } from '@storybook/web-components';
+import customElements from '../custom-elements.json';
+
+setCustomElements(customElements);
+
+// window.__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__ = customElementsManifest;
+
 export const parameters = {
     passArgsFirst: false,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
   };
+
