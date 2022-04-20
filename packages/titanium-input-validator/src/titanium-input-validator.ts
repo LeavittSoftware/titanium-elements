dfe84765d5a00e6a61bfa5248e86e076d154ea19
Kasey Hinton <kaseyjameshinton@gmail.com>
2022-04-20 13:21:04 -0600

    feat: adds titanium-input-validator

---
+++ packages/titanium-input-validator/src/titanium-input-validator.ts
@@ -0,0 +1,56 @@
+import { css, html, LitElement } from 'lit';
+import { property, customElement, state } from 'lit/decorators.js';
+
+@customElement('titanium-input-validator')
+export class TitaniumInputValidatorElement extends LitElement {
+  @property({ type: String }) validationMessage = '';
+  @state() evaluator = () => true;
+
+  @state() protected isUiValid = true;
+
+  firstUpdated() {
+    this.addEventListener(NotifyUserInputEvent.eventName, e => {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      this.reportValidity();
+    });
+  }
+
+  checkValidity(): boolean {
+    return !!this.evaluator();
+  }
+
+  reportValidity(): boolean {
+    const isValid = this.checkValidity();
+    this.isUiValid = isValid;
+    return isValid;
+  }
+
+  static styles = css`
+    :host {
+      display: inline-flex;
+      flex-direction: column;
+    }
+
+    error-message {
+      color: var(--mdc-theme-error, #b00020);
+      letter-spacing: 0.0333333em;
+      font-size: 12px;
+      height: 19px;
+      padding: 0 16px;
+      opacity: 0.99;
+      -webkit-font-smoothing: antialiased;
+    }
+  `;
+
+  render = () => html`<slot></slot>
+    <error-message>${this.isUiValid ? '' : this.validationMessage}</error-message> `;
+}
+
+export class NotifyUserInputEvent extends Event {
+  static eventName = 'user-input';
+
+  constructor() {
+    super(NotifyUserInputEvent.eventName, { bubbles: true, composed: true });
+  }
+}
