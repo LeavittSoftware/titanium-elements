f72dee0eaf78d6fbf3cedc53ae537ccac64350f7
Kasey Hinton <kaseyjameshinton@gmail.com>
2022-04-20 14:30:33 -0600

    docs(titanium-input-validator): adds JSDoc

--- packages/titanium-input-validator/src/titanium-input-validator.ts
+++ packages/titanium-input-validator/src/titanium-input-validator.ts
@@ -1,9 +1,27 @@
 import { css, html, LitElement } from 'lit';
 import { property, customElement, state } from 'lit/decorators.js';
 
+/**
+ * Input validator to make components use validation consistent with mwc-components.
+ *
+ * @element titanium-input-validator
+ * @slot any element - The slotted element should fire the NotifyUserInputEvent when it is ready to be validated
+ * @example
+ * <titanium-input-validator .evaluator=${() => (this.collection?.length ?? 0) > 0} validationMessage="Collection must have one item">
+ *     <custom-input @input=${() => this.dispatchEvent(new NotifyUserInputEvent())}></custom-input>
+ * </titanium-input-validator>
+ * @exports NotifyUserInputEvent - this is a custom event that the consumer of this component can fire to trigger validation
+ * @cssprop {Color} --mdc-theme-error - error color used for validation message
+ */
 @customElement('titanium-input-validator')
 export class TitaniumInputValidatorElement extends LitElement {
+  /**
+   *  Validation message displayed when the input is invalid
+   */
   @property({ type: String }) validationMessage = '';
+  /**
+   *  Evaluator function that will be called to determine if the input is valid
+   */
   @property({ type: Object }) evaluator = () => true;
 
   @state() protected isUiValid = true;
