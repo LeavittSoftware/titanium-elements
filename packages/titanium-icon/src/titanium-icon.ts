e28ab6c6c384781b3f0151b6c916d4b6551eef7d
Kasey Hinton <kaseyjameshinton@gmail.com>
2022-04-21 13:41:16 -0600

    docs(titanium-icon): adds jsdoc and story

--- packages/titanium-icon/src/titanium-icon.ts
+++ packages/titanium-icon/src/titanium-icon.ts
@@ -1,8 +1,21 @@
 import { customElement, html, LitElement, property } from 'lit-element';
 import { styles } from '@material/mwc-icon/mwc-icon-host.css';
 
+/**
+ * Titanium icon
+ *
+ * @element titanium-icon
+ * @example
+ * <mwc-list-item graphic="avatar" value=${o.Id ?? ''}>
+ *   <titanium-icon category slot="graphic" icon="category"></titanium-icon>
+ *   <span>${o.Name}</span>
+ * </mwc-list-item>`
+ */
 @customElement('titanium-icon')
 export class TitaniumIconElement extends LitElement {
+  /**
+   *  Icon that will be displayed. Select from the list of Google material icons.
+   */
   @property({ type: String }) icon: string = '';
 
   static styles = styles;
