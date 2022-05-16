d90f71aff53f549b8e5786e669100142118a0b9a
Aaron Drabeck <aaron-drabeck@leavitt.com>
2022-05-16 17:03:39 -0600

    fix(leavittbook): spelling and links

--- packages/leavittbook/src/getting-started.ts
+++ packages/leavittbook/src/getting-started.ts
@@ -28,20 +28,21 @@ export default class GettingStartedElement extends LitElement {
       <titanium-header header="Getting started" subHeader="Leavitt Group open-source custom elements" no-nav></titanium-header>
       <h2>TLDR;</h2>
       <p>
-        A collection of mostly <a href="https://lit.dev/">Lit HTML</a> based custom web elements to supplement or extend
-        <a href="https://github.com/material-components/material-web">Google's Material Web elements.</a>. These elements are open-source and free for anyone to
-        use. The elements prefixed with <code>titanium-</code> are less specific general use components. The elements prefixed with <code>leavitt-</code> are
-        more specific to the Leavitt Group, many require authentication, and therefor their use is limited to the Leavitt Group and its affiliates.
+        A collection of mostly <a target="_blank" href="https://lit.dev/">Lit HTML</a> based custom web elements to supplement or extend
+        <a target="_blank" href="https://github.com/material-components/material-web">Google's Material Web elements.</a>. These elements are open-source and
+        free for anyone to use. The elements prefixed with <code>titanium-</code> are less specific general use components. The elements prefixed with
+        <code>leavitt-</code> are more specific to the Leavitt Group, many require authentication, and therefore their use is limited to the Leavitt Group and
+        its affiliates.
       </p>
 
       <h2>Support</h2>
       <p>Chrome, Safari, Firefox, and Edge</p>
 
       <h2>Installation</h2>
-      <p>NPN install the element you want to use:</p>
+      <p>NPM install the element you want to use:</p>
       <code-block hide-open-button language="bash" open .snippet=${html`npm i @leavittsoftware/titanium-chip`}></code-block>
-      <p>Include the element on you page.</p>
-      <code-block hide-open-button open .snippet=${html`import '@microsoft/titanium-chip';`}></code-block>
+      <p>Include the element on your page.</p>
+      <code-block hide-open-button open .snippet=${html`import '@leavittsoftware/titanium-chip';`}></code-block>
       <p>Use the element:</p>
       <code-block hide-open-button open .snippet=${html`<titanium-chip></titanium-chip>`}></code-block>
     `;
