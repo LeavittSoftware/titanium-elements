ee4e2ccf60f97f5898270b73d27dca33d2b5d433
Carly <cxrlycue@gmail.com>
2022-05-18 16:04:18 -0600

    fix(leavittbook): missing playground css

--- packages/leavittbook/src/demos/titanium-data-table-header/titanium-data-table-header-playground.ts
+++ packages/leavittbook/src/demos/titanium-data-table-header/titanium-data-table-header-playground.ts
@@ -10,20 +10,37 @@ import '@material/mwc-icon';
 /* playground-fold */
 @customElement('titanium-data-table-header-playground')
 export class TitaniumDataTableHeaderPlayground extends LitElement {
-  static styles = [h1, p, css``];
+  static styles = [
+    h1,
+    p,
+    css`
+      :host {
+        display: flex;
+        flex-direction: column;
+        --mdc-icon-font: 'Material Icons Outlined';
+        margin: 24px 12px;
+      }
+
+      div {
+        margin: 24px 0 36px 0;
+      }
+    `,
+  ];
 
   render() {
     /* playground-fold-end */
     return html`
       <h1>Default</h1>
       <p>Examples using left, center, right, large, no-sort, desktop, and width attributes</p>
-      <titanium-data-table-header left column-name="Left" title="Left"></titanium-data-table-header>
-      <titanium-data-table-header center column-name="Center" title="Center"></titanium-data-table-header>
-      <titanium-data-table-header right column-name="Right" title="Right"></titanium-data-table-header>
-      <titanium-data-table-header large column-name="Large" title="Large"></titanium-data-table-header>
-      <titanium-data-table-header no-sort column-name="NoSort" title="No sort"></titanium-data-table-header>
-      <titanium-data-table-header desktop column-name="Desktop" title="Desktop"></titanium-data-table-header>
-      <titanium-data-table-header width="200px" column-name="Width" title="Width"></titanium-data-table-header>
+      <div>
+        <titanium-data-table-header left column-name="Left" title="Left"></titanium-data-table-header>
+        <titanium-data-table-header center column-name="Center" title="Center"></titanium-data-table-header>
+        <titanium-data-table-header right column-name="Right" title="Right"></titanium-data-table-header>
+        <titanium-data-table-header large column-name="Large" title="Large"></titanium-data-table-header>
+        <titanium-data-table-header no-sort column-name="NoSort" title="No sort"></titanium-data-table-header>
+        <titanium-data-table-header desktop column-name="Desktop" title="Desktop"></titanium-data-table-header>
+        <titanium-data-table-header width="200px" column-name="Width" title="Width"></titanium-data-table-header>
+      </div>
     `;
   }
 }
