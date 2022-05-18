a434910eb00968ae6b9972bd928a5c57b16ad319
Aaron Drabeck <aaron-drabeck@leavitt.com>
2022-05-18 13:23:31 -0600

    fix: import

--- packages/leavittbook/src/demos/mwc-datefield/mwc-datefield-playground.ts
+++ packages/leavittbook/src/demos/mwc-datefield/mwc-datefield-playground.ts
@@ -1,5 +1,5 @@
 /* playground-fold */
-import { css, html, LitElement, PropertyValueMap } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { customElement, query, queryAll, state } from 'lit/decorators.js';
 import { h1, p } from '@leavittsoftware/titanium-styles';
 import '@material/mwc-switch';
