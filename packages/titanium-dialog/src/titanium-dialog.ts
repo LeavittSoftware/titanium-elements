74f58b424ea728f2862b4fba36bfdea1f87c0460
Aaron Drabeck <aaron-drabeck@leavitt.com>
2022-06-02 17:00:56 -0600

    feat(titanium-dialog): converts dialog base to use native HTML dialog
    BREAKING CHANGE: content slot removed, use default slot. 
    Adds primaryAction and secondaryAction button slots; adds disable-scroll attribute; add new css props for min/max-heights and widths.

--- packages/titanium-dialog/src/titanium-dialog.ts
+++ packages/titanium-dialog/src/titanium-dialog.ts
@@ -1,23 +1,35 @@
 import '@material/mwc-button';
 import { css, html } from 'lit';
 import { property, customElement, query } from 'lit/decorators.js';
-import { TitaniumDialogBaseElement } from './titanium-dialog-base';
 import { h1, p } from '@leavittsoftware/titanium-styles';
+import { TitaniumNativeDialogBaseElement } from './titanium-native-dialog-base';
 
 /**
  * Material design inspired dialog
  *
  * @element titanium-dialog
  *
- * @slot content - dialog content
- * @slot actions - dialog buttons (if provided will overwrite the default close button)
+ * @fires opening - Fired when opening animation begins
+ * @fires opened - Fired after open animation is complete
+ * @fires closing - Fired when closing animation begins
+ * @fires closed - Fired after closing animation is complete
+ * @fires removed - Fired when element is removed from the DOM
  *
- * @cssprop {Color} [--titanium-dialog-background-color=#ffffff] - Background color of the dialog
- * @cssprop {<length> | <percentage> | none | max-content | min-content | fit-content | fill-available} [--titanium-dialog-max-width=none] - Max-width of dialog container
+ * @slot content - dialog content
+ * @slot actions - dialog buttons (if provided will overwrite primaryAction and secondaryAction slots)
+ * @slot primaryAction - primaryDialog button slot (if provided will overwrite the default close button)
+ * @slot secondaryAction - secondaryAction button slot
+ *
+ * @cssprop  [--titanium-dialog-background-color=#ffffff] - Background color of the dialog
+ * @cssprop {<length> | <percentage> | none | max-content | min-content | fit-content | fill-available} [--titanium-dialog-max-width=calc(100vh - 32px)] - max-width of dialog
+ * @cssprop {<length> | <percentage> | none | max-content | min-content | fit-content | fill-available} [--titanium-dialog-min-width=none] - min-width of dialog
+ * @cssprop {<length> | <percentage> | none | max-content | min-content | fit-content | fill-available} [--titanium-dialog-max-height=none] - max-height of dialog
+ * @cssprop {<length> | <percentage> | none | max-content | min-content | fit-content | fill-available} [--titanium-dialog-min-height=calc(100vh - 32px)] - min-height of dialog
  * @cssprop {Color} [--app-border-color=#dadce0] - Dialog border color
+ *
  */
 @customElement('titanium-dialog')
-export class TitaniumDialogElement extends TitaniumDialogBaseElement {
+export class TitaniumDialogElement extends TitaniumNativeDialogBaseElement {
   /**
    * Dialog title text
    */
@@ -29,31 +41,46 @@ export class TitaniumDialogElement extends TitaniumDialogBaseElement {
   @property({ type: Boolean, reflect: true }) disableSmoothScroll: boolean = false;
 
   /**
-   * Allows dialog content to scroll.  (automatically controlled based on content and viewport size)
+   * Prevents dialog content from scrolling.
    */
+  @property({ type: Boolean, reflect: true, attribute: 'disable-scroll' }) protected disableScroll: boolean = false;
+
   @property({ type: Boolean, reflect: true }) protected scrolls: boolean = false;
+  @query('main', true) protected mainElement!: HTMLElement;
 
-  @query('section') section: HTMLElement;
+  async firstUpdated() {
+    await super.firstUpdated();
+    const observer = new ResizeObserver(() => {
+      if (!this.disableScroll) {
+        this.scrolls = this.mainElement.scrollHeight > this.mainElement.offsetHeight;
+      }
+    });
 
-  afterOpen() {
-    // Experimental way to determine if scroll bar is present.
-    // Prevents user from having to declare if modal is scrollable.
-    // TODO: Use Resize observers in the future......
-    this.scrolls = this.section.scrollHeight > this.section.offsetHeight;
+    observer.observe(this.mainElement);
   }
 
   static styles = [
-    ...TitaniumDialogBaseElement.styles,
+    ...TitaniumNativeDialogBaseElement.styles,
     h1,
     p,
     css`
-      h1 {
-        display: block;
-        padding: 24px 24px 16px 24px;
+      dialog {
+        grid:
+          'header'
+          'content' 1fr
+          'footer';
       }
 
-      section[paragraph] {
-        padding: 8px 24px 16px 24px;
+      h1 {
+        display: block;
+        padding: 18px 24px 9px;
+      }
+
+      main {
+        padding: 0 24px 16px 24px;
+      }
+
+      :host(:not([disable-scroll])) main {
         overflow-y: auto;
       }
 
@@ -62,7 +89,7 @@ export class TitaniumDialogElement extends TitaniumDialogBaseElement {
         padding: 16px 24px;
       }
 
-      :host([scrolls]:not([disableSmoothScroll])) section {
+      :host([scrolls]:not([disableSmoothScroll])) main {
         -webkit-overflow-scrolling: touch;
       }
 
@@ -85,24 +112,14 @@ export class TitaniumDialogElement extends TitaniumDialogBaseElement {
   renderSlot() {
     return html`
       <h1>${this.header}</h1>
-      <section paragraph>
-        <slot name="content"></slot>
-      </section>
+      <main>
+        <slot></slot>
+      </main>
 
       <footer>
         <slot name="actions">
-          ${this.focusTrap
-            ? ''
-            : html`
-                <mwc-button
-                  @click=${(e: Event) => {
-                    e.preventDefault();
-                    this.close('close-click');
-                  }}
-                  label="CLOSE"
-                >
-                </mwc-button>
-              `}
+          <slot name="secondaryAction"></slot>
+          <slot name="primaryAction"><mwc-button @click=${() => this.close('close')} label="CLOSE"></mwc-button></slot>
         </slot>
       </footer>
     `;
