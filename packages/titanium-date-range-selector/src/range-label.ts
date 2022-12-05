88b87ac6ed3673526409428fd6cc2444caea989e
Aaron Drabeck <aaron-drabeck@leavitt.com>
2022-12-05 13:50:40 -0700

    feat: add a range label helper

---
+++ packages/titanium-date-range-selector/src/range-label.ts
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs/esm';
+import { DateRangeKey, DateRanges } from './titanium-date-range-selector';
+
+export function rangeLabel(range: DateRangeKey, startDate: string | null, endDate: string | null, prefix = 'Created') {
+  if (range !== 'custom') {
+    return `${prefix} ${DateRanges.get(range)?.name?.toLowerCase() ?? ''}`;
+  } else {
+    if (startDate && endDate) {
+      return `${prefix} ${dayjs(startDate).format('MMM D, YYYY')} to ${dayjs(endDate).format('MMM D, YYYY')}`;
+    }
+    return endDate ? `${prefix} before ${dayjs(endDate).format('MMM D, YYYY')}` : `${prefix} after ${dayjs(startDate).format('MMM D, YYYY')}`;
+  }
+}
