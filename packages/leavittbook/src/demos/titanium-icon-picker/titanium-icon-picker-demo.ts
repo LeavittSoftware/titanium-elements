2e9b0a7dcc525f99a3b4f5bff84a74b20b92ce01
Aaron Drabeck <aaron-drabeck@leavitt.com>
2023-08-17 12:18:19 -0600

    feat: adds titanium-icon-picker w/demo

---
+++ packages/leavittbook/src/demos/titanium-icon-picker/titanium-icon-picker-demo.ts
@@ -0,0 +1,24 @@
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import StoryStyles from '../../styles/story-styles';
+import '../../shared/story-header';
+
+import '@api-viewer/docs';
+import '../../shared/smart-demo';
+import './titanium-icon-picker-playground';
+
+@customElement('titanium-icon-picker-demo')
+export class TitaniumIconPickerDemoElement extends LitElement {
+  static styles = [StoryStyles, css``];
+
+  render() {
+    return html`
+      <story-header name="Titanium icon picker" packageName="titanium-icon-picker" className="TitaniumIconPicker"></story-header>
+      <smart-demo line-numbers resizable project-src="../src/demos/titanium-icon-picker/project.json"
+        ><titanium-icon-picker-playground></titanium-icon-picker-playground>
+      </smart-demo>
+      <api-docs src="./custom-elements.json" selected="titanium-icon-picker"></api-docs>
+    `;
+  }
+}
---
+++ packages/titanium-icon-picker/src/titanium-icon-picker.ts
@@ -0,0 +1,99 @@
+import { html, LitElement } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import '@material/mwc-select';
+import '@leavittsoftware/titanium-icon';
+import '@material/mwc-list/mwc-list-item';
+import { MaterialIcon, MaterialIcons } from './material-icons';
+import { Select } from '@material/mwc-select';
+
+/**
+ * Titanium icon picker - a picker for material icons
+ *
+ * @element titanium-icon-picker
+ *
+ */
+@customElement('titanium-icon-picker')
+export class TitaniumIconPicker extends LitElement {
+  /**
+   *  The selected icon
+   */
+  @property({ type: String }) value: MaterialIcon | null = null;
+
+  /**
+   *  Sets floating label value.
+   */
+  @property({ type: String }) label: string = 'Icon';
+
+  /**
+   *  Sets placeholder text value.
+   */
+  @property({ type: String }) placeholder: string = '';
+
+  /**
+   *  Whether or not the input should be disabled.
+   */
+  @property({ type: Boolean }) disabled: boolean = false;
+
+  /**
+   *  Displays error state if input is empty and input is blurred.
+   */
+  @property({ type: Boolean }) required: boolean = false;
+
+  @query('mwc-select') protected select: Select & { mdcFoundation: { setValid(): boolean }; isUiValid: boolean };
+
+  async reset() {
+    this.value = null;
+    await this.updateComplete;
+    if (this.select) {
+      this.select.value = '';
+      this.select.isUiValid = true;
+      this.select.mdcFoundation?.setValid?.(true);
+    }
+  }
+
+  /**
+   *  Runs layout() method on textfield.
+   */
+  layout() {
+    this.select.layout();
+  }
+
+  /**
+   *  Sets focus on the input.
+   */
+  focus() {
+    this.select.focus();
+  }
+
+  /**
+   *  Returns true if the input passes validity checks.
+   */
+  checkValidity() {
+    return this.select.checkValidity();
+  }
+
+  /**
+   *  Runs checkValidity() method, and if it returns false, then it reports to the user that the input is invalid.
+   */
+  reportValidity() {
+    return this.select.reportValidity();
+  }
+
+  render = () =>
+    html` <mwc-select
+      outlined
+      .label=${this.label}
+      .disabled=${this.disabled}
+      .placeholder=${this.placeholder}
+      .required=${this.required}
+      .icon=${this.value || ''}
+      .value=${this.value ?? ''}
+      @selected=${e => (this.value = e.target.value || null)}
+    >
+      ${MaterialIcons.map(
+        (icon: string) => html`
+          <mwc-list-item graphic="icon" value=${icon}><titanium-icon slot="graphic" icon=${icon}></titanium-icon> ${icon}</mwc-list-item>
+        `
+      )}
+    </mwc-select>`;
+}
