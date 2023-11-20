eba283e7c99c45869b6762be9b88d60e7519dd0f
Aaron Drabeck <aaron-drabeck@leavitt.com>
2023-11-20 13:12:52 -0700

    feat: use native popover API for full page indicator

--- packages/web/titanium/full-page-loading-indicator/full-page-loading-indicator.ts
+++ packages/web/titanium/full-page-loading-indicator/full-page-loading-indicator.ts
@@ -1,7 +1,9 @@
 import { css, html, LitElement } from 'lit';
-import { property, customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import { PendingStateEvent } from '../types/pending-state-event';
+
 import '@material/web/progress/linear-progress';
+
 /**
  * A simple full-screen veil with loading indicator that uses promise driven pending-state-events
  *
@@ -11,12 +13,8 @@ import '@material/web/progress/linear-progress';
  */
 @customElement('titanium-full-page-loading-indicator')
 export class TitaniumFullPageLoadingIndicator extends LitElement {
-  @property({ type: Boolean, reflect: true }) protected accessor opened: boolean;
-  @property({ type: Boolean, reflect: true }) protected accessor opening: boolean;
-  @property({ type: Boolean, reflect: true }) protected accessor closing: boolean;
+  @state() private accessor open: boolean;
 
-  #animationTimer: number;
-  #animationFrame: number;
   #openDelayTimer: number;
   #closeDelayTimer: number;
 
@@ -30,6 +28,9 @@ export class TitaniumFullPageLoadingIndicator extends LitElement {
   #openCount = 0;
 
   firstUpdated() {
+    this.popover = 'manual';
+    this.addEventListener('toggle', (e: ToggleEvent) => (this.open = e.newState === 'open'));
+
     window.addEventListener(PendingStateEvent.eventType, async (e: PendingStateEvent) => {
       this.#open();
       this.#openCount++;
@@ -53,21 +54,8 @@ export class TitaniumFullPageLoadingIndicator extends LitElement {
 
     this.#openDelayTimer = window.setTimeout(() => {
       this.#timeOpen = performance.now();
-      this.closing = false;
-      this.opened = false;
-      this.opening = true;
-
+      this.showPopover();
       document.body.style.overflow = 'hidden';
-
-      this.#runNextAnimationFrame_(() => {
-        if (this.closing) {
-          return;
-        }
-        this.opened = true;
-        this.#animationTimer = window.setTimeout(() => {
-          this.#handleAnimationTimerEnd_();
-        }, 50);
-      });
     }, this.#openDelay);
   }
 
@@ -77,76 +65,36 @@ export class TitaniumFullPageLoadingIndicator extends LitElement {
     const closeDelay = Math.max(this.#minTimeOpen - totalTimeOpened, 0);
 
     this.#closeDelayTimer = window.setTimeout(() => {
-      cancelAnimationFrame(this.#animationFrame);
-      this.#animationFrame = 0;
-
-      this.closing = true;
-      this.opened = false;
-      this.opening = false;
-      clearTimeout(this.#animationTimer);
-      this.#animationTimer = window.setTimeout(() => {
-        this.#handleAnimationTimerEnd_();
-        document.body.style.overflow = '';
-      }, 150);
+      this.hidePopover();
+      document.body.style.overflow = '';
     }, closeDelay);
-  }
-
-  #handleAnimationTimerEnd_() {
-    this.opening = false;
-    this.closing = false;
-  }
-
-  /**
-   * Runs the given logic on the next animation frame, using setTimeout to factor in Firefox reflow behavior.
-   */
-  #runNextAnimationFrame_(callback: () => void) {
-    cancelAnimationFrame(this.#animationFrame);
-    this.#animationFrame = requestAnimationFrame(() => {
-      this.#animationFrame = 0;
-      clearTimeout(this.#animationFrame);
-      this.#animationFrame = window.setTimeout(callback, 0);
-    });
   }
 
   static styles = css`
     :host {
-      display: none;
-      position: relative;
-      position: fixed;
-      z-index: 19;
-      top: 0;
-      left: 0;
       width: 100%;
       height: 100%;
-      opacity: 0;
-      background: var(--md-sys-color-scrim, #000);
-      -webkit-transition: opacity 75ms linear;
-      -o-transition: opacity 75ms linear;
-      transition: opacity 75ms linear;
+      max-width: 100vw;
+      max-height: 100vh;
+
+      border: 0;
+      inset: unset;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      margin: 0;
+      padding: 0;
+      background: transparent;
     }
 
-    :host([opening]),
-    :host([closing]),
-    :host([opened]) {
-      display: block;
-      z-index: 7;
-    }
-
-    :host([opening]) {
-      -webkit-transition: opacity 50ms linear;
-      -o-transition: opacity 50ms linear;
-      transition: opacity 50ms linear;
-    }
-
-    :host([closing]) {
-      -webkit-transition: opacity 150ms linear;
-      -o-transition: opacity 150ms linear;
-      transition: opacity 150ms linear;
-    }
-
-    :host([opened]) {
-      opacity: 0.32;
+    :host::backdrop {
+      background-color: var(--md-sys-color-scrim, #000);
       backdrop-filter: blur(6px);
+    }
+
+    :host(:popover-open)::backdrop {
+      opacity: 0.32;
     }
 
     md-linear-progress {
@@ -159,6 +107,6 @@ export class TitaniumFullPageLoadingIndicator extends LitElement {
   `;
 
   render() {
-    return html` <md-linear-progress ?indeterminate=${this.opened} ?hide=${!this.opening && !this.closing && !this.opened}></md-linear-progress> `;
+    return html` <md-linear-progress ?indeterminate=${this.open} ?hide=${!this.open}></md-linear-progress> `;
   }
 }
