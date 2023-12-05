b810e8fd83287e95a2f8e88b5e6d80d2535c0f4e
Aaron Drabeck <aaron-drabeck@leavitt.com>
2023-12-05 09:48:11 -0700

    feat(icons): new demo UI; dark mode compatible icons

--- packages/leavittbook/src/demos/available-cdn-icons/available-cdn-icons-playground.ts
+++ packages/leavittbook/src/demos/available-cdn-icons/available-cdn-icons-playground.ts
@@ -2,12 +2,95 @@
 import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { h1, p } from '@leavittsoftware/web/titanium/styles/styles';
+import { TitaniumSnackbarSingleton } from '@leavittsoftware/web/titanium/snackbar/snackbar';
+
+import '@material/web/ripple/ripple';
+import '@material/web/focus/md-focus-ring';
 
 /* playground-fold-end */
 
 /* playground-fold */
 @customElement('available-cdn-icons-playground')
 export class AvailableCdnIconsPlayground extends LitElement {
+  icons = [
+    'icon-app-accounts.svg',
+    'icon-app-acm.svg',
+    'icon-app-amc.svg',
+    'icon-app-apps.svg',
+    'icon-app-broker-key.svg',
+    'icon-app-calc.svg',
+    'icon-app-company-manager.svg',
+    'icon-app-conference.svg',
+    'icon-app-crm-to-ams360.svg',
+    'icon-app-cust-search.svg',
+    'icon-app-departments.svg',
+    'icon-app-distribution.svg',
+    'icon-app-domo.svg',
+    'icon-app-duplicate.svg',
+    'icon-app-education.svg',
+    'icon-app-email.svg',
+    'icon-app-employee-lookup.svg',
+    'icon-app-employee-manager.svg',
+    'icon-app-farmer.svg',
+    'icon-app-game-cl.svg',
+    'icon-app-game-pl.svg',
+    'icon-app-gov-prin.svg',
+    'icon-app-home.svg',
+    'icon-app-investors.svg',
+    'icon-app-lamp.svg',
+    'icon-app-leases.svg',
+    'icon-app-leavitt-link.svg',
+    'icon-app-marketing.svg',
+    'icon-app-naics.svg',
+    'icon-app-pcms.svg',
+    'icon-app-permissions.svg',
+    'icon-app-phones.svg',
+    'icon-app-prod-budget.svg',
+    'icon-app-reporting-tools.svg',
+    'icon-app-service-timeline.svg',
+    'icon-app-stationary.svg',
+    'icon-app-surge.svg',
+    'icon-app-survey.svg',
+    'icon-app-tv.svg',
+    'icon-app-vital.svg',
+    'icon-biz-card.svg',
+    'icon-carousel.svg',
+    'icon-company-manager.svg',
+    'icon-delete-logo.svg',
+    'icon-department.svg',
+    'icon-dev-sales.svg',
+    'icon-domo.svg',
+    'icon-email-initial.svg',
+    'icon-email-recipients.svg',
+    'icon-email-reminder.svg',
+    'icon-employees.svg',
+    'icon-enterprise.svg',
+    'icon-event-details.svg',
+    'icon-icon-logs.svg',
+    'icon-logo.svg',
+    'icon-log-parser.svg',
+    'icon-marketing.svg',
+    'icon-meetings.svg',
+    'icon-name-division.svg',
+    'icon-new-hire.svg',
+    'icon-onboarding.svg',
+    'icon-participants.svg',
+    'icon-pathways.svg',
+    'icon-pcms.svg',
+    'icon-permissions.svg',
+    'icon-phone.svg',
+    'icon-prod-budgets.svg',
+    'icon-program.svg',
+    'icon-reminder.svg',
+    'icon-reporting-tools.svg',
+    'icon-rules.svg',
+    'icon-sales-culture.svg',
+    'icon-scoreboard-details.svg',
+    'icon-scoreboard-prizes.svg',
+    'icon-success.svg',
+    'icon-template.svg',
+  ];
+
   static styles = [
     h1,
     p,
@@ -19,14 +102,27 @@ export class AvailableCdnIconsPlayground extends LitElement {
         margin: 24px 12px;
       }
 
-      div {
+      all-icons {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+        gap: 12px;
+      }
+
+      button {
+        cursor: pointer;
+        color: inherit;
+        background-color: inherit;
+        border: none;
+        display: flex;
+        position: relative;
+        flex-direction: column;
+        align-items: center;
+        gap: 12px;
         border: 1px solid var(--md-sys-color-outline);
         padding: 24px;
         border-radius: 8px;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 12px;
-        margin: 24px 0 36px 0;
+        --md-focus-ring-shape: 8px;
+        text-align: center;
       }
     `,
   ];
@@ -34,96 +130,22 @@ export class AvailableCdnIconsPlayground extends LitElement {
   render() {
     /* playground-fold-end */
     return html`
-      <h1>Available CDN icons</h1>
-      <div>
-        <img src="https://cdn.leavitt.com/icons/icon-logo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-scoreboard-prizes.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-delete-logo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-rules.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-scoreboard-details.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-carousel.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-name-division.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-company-manager.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-domo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-employees.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-log-parser.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-pcms.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-permissions.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-prod-budgets.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-email-recipients.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-event-details.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-participants.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-accounts.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-acm.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-amc.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-apps.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-broker-key.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-calc.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-company-manager.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-conference.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-crm-to-ams360.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-cust-search.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-departments.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-distribution.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-domo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-duplicate.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-education.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-email.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-employee-lookup.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-employee-manager.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-farmer.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-game-cl.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-game-pl.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-gov-prin.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-home.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-investors.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-lamp.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-leases.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-leavitt-link.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-marketing.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-naics.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-pcms.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-permissions.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-phones.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-prod-budget.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-reporting-tools.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-service-timeline.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-stationary.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-surge.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-survey.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-tv.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-app-vital.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-carousel.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-delete-logo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-department.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-email-initial.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-email-reminder.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-employees.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-enterprise.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-event-details.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-icon-logs.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-logo.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-marketing.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-name-division.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-pcms.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-phone.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-prod-budgets.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-reminder.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-reporting-tools.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-rules.svg   " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-scoreboard-details.svg" height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-scoreboard-prizes.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-success.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-biz-card.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-template.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-dev-sales.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-meetings.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-new-hire.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-onboarding.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-pathways.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-program.svg " height="48" />
-        <img src="https://cdn.leavitt.com/icons/icon-sales-culture.svg " height="48" />
-      </div>
+      <all-icons>
+        ${this.icons.map(
+          (o) =>
+            html`<button
+              @click=${() => {
+                navigator.clipboard.writeText(`https://cdn.leavitt.com/icons/${o}`);
+                TitaniumSnackbarSingleton?.open('URL copied to clipboard!', { autoHide: true });
+              }}
+            >
+              <md-ripple></md-ripple>
+              <md-focus-ring></md-focus-ring>
+              <img src="https://cdn.leavitt.com/icons/${o}" height="92" />
+              <div>${o}</div>
+            </button>`
+        )}
+      </all-icons>
     `;
   }
 }
