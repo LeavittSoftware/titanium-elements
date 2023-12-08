db4cc95243b2bd506a39a98f1846c7f58281c3be
Aaron Drabeck <aaron-drabeck@leavitt.com>
2023-12-08 11:18:10 -0700

    feat(toolbar): use md-elevation; ff support; refactor code

--- packages/web/titanium/toolbar/toolbar.ts
+++ packages/web/titanium/toolbar/toolbar.ts
@@ -1,5 +1,8 @@
 ﻿import { css, html, LitElement } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+
+import '@material/web/elevation/elevation';
+
 /**
  * A material design inspired site toolbar
  *
@@ -38,14 +41,12 @@ export class TitaniumToolbar extends LitElement {
       padding: 0 14px;
       background-color: var(--md-sys-color-background);
       color: var(--md-sys-color-on-background);
+
+      --md-elevation-level: 0;
     }
 
     :host([shadow]) {
-      box-shadow:
-        0px 2px 4px -1px rgb(from var(--md-sys-color-on-background) r g b / 0.2),
-        0px 4px 5px 0px rgb(from var(--md-sys-color-on-background) r g b / 0.14),
-        0px 1px 10px 0px rgb(from var(--md-sys-color-on-background) r g b / 0.12);
-      transition: box-shadow 0.3s ease-in-out;
+      --md-elevation-level: 3;
     }
 
     ::slotted([main-title]) {
@@ -66,37 +67,28 @@ export class TitaniumToolbar extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    document.addEventListener('scroll', this.onScroll.bind(this), false);
+    document.addEventListener('scroll', this.#onScroll.bind(this), false);
   }
 
   disconnectedCallback() {
-    document.removeEventListener('scroll', this.onScroll, false);
+    document.removeEventListener('scroll', this.#onScroll, false);
     super.disconnectedCallback();
   }
 
-  /**
-   * @ignore
-   */
-  onScroll() {
-    this.amountScrolled();
+  #onScroll() {
+    this.#amountScrolled();
   }
 
-  /**
-   * @ignore
-   */
-  amountScrolled() {
+  #amountScrolled() {
     const winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight;
-    const docHeight = this.getDocHeight();
-    const scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    const docHeight = this.#getDocHeight();
+    const scrollTop = window.scrollY || (document.documentElement || document.body.parentNode || document.body).scrollTop;
     const trackLength = docHeight - winHeight;
     const pctScrolled = Math.floor((scrollTop / trackLength) * 100);
     this.shadow = (pctScrolled || 0) !== 0;
   }
 
-  /**
-   * @ignore
-   */
-  getDocHeight() {
+  #getDocHeight() {
     const D = document;
     return Math.max(
       D.body.scrollHeight,
@@ -109,6 +101,6 @@ export class TitaniumToolbar extends LitElement {
   }
 
   render() {
-    return html` <slot></slot> `;
+    return html` <md-elevation></md-elevation><slot></slot>`;
   }
 }
