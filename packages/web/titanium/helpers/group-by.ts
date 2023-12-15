a65cc8c9467df67e903f17aeab13ab437e535677
Aaron Drabeck <aaron-drabeck@leavitt.com>
2023-12-15 12:11:42 -0700

    feat(helpers): add group-by helper

---
+++ packages/web/titanium/helpers/group-by.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function groupBy<T, K extends keyof any>(arr: T[], key: (i: T) => K) {
+  return arr.reduce(
+    (groups, item) => {
+      (groups[key(item)] ||= []).push(item);
+      return groups;
+    },
+    {} as Record<K, T[]>
+  );
+}
