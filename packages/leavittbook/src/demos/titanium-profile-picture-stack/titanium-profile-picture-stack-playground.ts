bf872420e02389373bca06ec022b99596b335abb
Kasey Hinton <kaseyjameshinton@gmail.com>
2024-03-04 10:27:24 -0700

    feat(titanium-profile-picture-stack): Adds profile picture stack element (#597)
    * Add profile picture stack
    
    * Add more demos. Add resize observer.
    
    * Clean up docs
    
    * reorder import
    
    * prettier
    
    * Addresses PR comments
    
    * handle one person use case
    
    * clean up playground
    
    * Add parts

---
+++ packages/leavittbook/src/demos/titanium-profile-picture-stack/titanium-profile-picture-stack-playground.ts
@@ -0,0 +1,95 @@
+/* playground-fold */
+import { css, html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { h1, p } from '@leavittsoftware/web/titanium/styles/styles';
+import { Person } from '@leavittsoftware/lg-core-typescript';
+
+/* playground-fold-end */
+import '@leavittsoftware/web/titanium/profile-picture-stack/profile-picture-stack';
+import '@leavittsoftware/web/titanium/card/card';
+import '@material/web/iconbutton/outlined-icon-button';
+import '@material/web/icon/icon';
+
+const kaseyPerson: Partial<Person> = {
+  Id: 771130,
+  FullName: 'Kasey Person',
+  ProfilePictureCdnFileName: '_hNIx5g5YkhcC1BCH_-lJBOlMy5urO1kMrmHl-DEyn15qs9IOnAzxXnlV9ed',
+} as Person;
+
+const randomPerson: Partial<Person> = { Id: 771130, FullName: 'Random Person', ProfilePictureCdnFileName: '' };
+
+/* playground-fold */
+@customElement('titanium-profile-picture-stack-playground')
+export class TitaniumProfilePictureStackPlayground extends LitElement {
+  @state() people: Array<Partial<Person>> = [randomPerson, kaseyPerson, randomPerson, kaseyPerson, randomPerson];
+  @state() manyPeople: Array<Partial<Person>> = new Array(25).fill(kaseyPerson);
+
+  static styles = [
+    h1,
+    p,
+    css`
+      :host {
+        display: flex;
+        flex-direction: column;
+
+        margin: 24px 12px;
+      }
+
+      titanium-profile-picture-stack {
+        margin: 0 0 24px 0;
+      }
+
+      titanium-profile-picture-stack[size='50'] {
+        --titanium-profile-picture-stack-transform-scale: 1.5;
+      }
+
+      titanium-card:first-of-type {
+        margin-bottom: 12px;
+      }
+
+      button-container {
+        align-items: center;
+        justify-self: end;
+        display: flex;
+        gap: 12px;
+      }
+    `,
+  ];
+
+  render() {
+    /* playground-fold-end */
+    return html`
+      <titanium-card>
+        <h1>Default</h1>
+        <titanium-profile-picture-stack .people=${this.people}></titanium-profile-picture-stack>
+
+        <h1>One person</h1>
+        <titanium-profile-picture-stack .people=${[kaseyPerson]}></titanium-profile-picture-stack>
+
+        <h1>Max of 3</h1>
+        <titanium-profile-picture-stack max="3" .people=${this.people}></titanium-profile-picture-stack>
+
+        <h1>Enable directory href</h1>
+        <titanium-profile-picture-stack enable-directory-href .people=${this.people}></titanium-profile-picture-stack>
+
+        <h1>Auto resize - Large</h1>
+        <titanium-profile-picture-stack auto-resize size="50" .people=${new Array(25).fill(kaseyPerson)}></titanium-profile-picture-stack>
+      </titanium-card>
+
+      <titanium-card>
+        <h1>Auto resize</h1>
+        <titanium-profile-picture-stack auto-resize .people=${this.manyPeople}></titanium-profile-picture-stack>
+
+        <button-container>
+          <p>${this.manyPeople?.length} ${this.manyPeople?.length === 1 ? 'person' : 'people'}</p>
+          <md-outlined-icon-button @click=${() => (this.manyPeople = [...this.manyPeople?.slice(0, this.manyPeople?.length - 1)])}>
+            <md-icon>remove</md-icon>
+          </md-outlined-icon-button>
+          <md-outlined-icon-button @click=${() => (this.manyPeople = [...this.manyPeople, kaseyPerson])}>
+            <md-icon>add</md-icon>
+          </md-outlined-icon-button>
+        </button-container>
+      </titanium-card>
+    `;
+  }
+}
