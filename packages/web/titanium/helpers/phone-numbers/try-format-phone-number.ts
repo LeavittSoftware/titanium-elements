36f374ac05dcafa476010e95cb3da7b73d7b1b93
Aaron Drabeck <github@aarondrabeck.com>
2025-05-14 14:12:03 -0600

    feat: add tryFormatPhoneNumber

---
+++ packages/web/titanium/helpers/phone-numbers/try-format-phone-number.ts
@@ -0,0 +1,8 @@
+export function tryormatPhoneNumber(phoneNumberString: string) {
+  const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+  return phoneNumberString;
+}
