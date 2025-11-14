661ae576b72b2a0c596d746daf6747c9654d99bc
Aaron Drabeck <github@aarondrabeck.com>
2025-11-14 09:21:12 -0700

    feat(helpers): add phone mask

---
+++ packages/web/titanium/helpers/phone-numbers/phone-mask.ts
@@ -0,0 +1,4 @@
+export function phoneMask(phoneNumber: string) {
+    const x = phoneNumber.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/) ?? '';
+    return !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+  }
