f8ec885364fc19232fcfac1ea7921be068f3a7b3
Kasey Hinton <kaseyjameshinton@gmail.com>
2025-12-23 13:30:55 -0700

    upgrade youtube demo

--- packages/leavittbook/src/demos/titanium-youtube-input-demo.ts
+++ packages/leavittbook/src/demos/titanium-youtube-input-demo.ts
@@ -1,7 +1,11 @@
 import '../shared/story-header';
 
+import '@leavittsoftware/web/leavitt/app/app-main-content-container';
+import '@leavittsoftware/web/leavitt/app/app-navigation-header';
+import '@leavittsoftware/web/leavitt/app/app-width-limiter';
+import '@material/web/divider/divider';
 import '@api-viewer/docs';
-import '@material/web/button/outlined-button';
+import '@material/web/button/filled-tonal-button';
 
 import { css, html, LitElement } from 'lit';
 import { customElement, query, queryAll } from 'lit/decorators.js';
@@ -9,6 +13,8 @@ import { h1, p } from '@leavittsoftware/web/titanium/styles/styles';
 import '@leavittsoftware/web/titanium/youtube-input/youtube-input';
 import { TitaniumYouTubeInput } from '@leavittsoftware/web/titanium/youtube-input/youtube-input';
 import { DOMEvent } from '@leavittsoftware/web/titanium/types/dom-event';
+import { heroStyles } from '../styles/hero-styles';
+
 import StoryStyles from '../styles/story-styles';
 
 @customElement('titanium-youtube-input-demo')
@@ -18,23 +24,31 @@ export class TitaniumYoutubeInputDemo extends LitElement {
 
   static styles = [
     StoryStyles,
+    heroStyles,
     h1,
     p,
     css`
       :host {
-        display: flex;
-        flex-direction: column;
-        margin: 24px 12px;
+        display: grid;
       }
 
-      div {
-        border: 1px solid var(--md-sys-color-outline);
+      main {
+        display: grid;
+        align-content: start;
+      }
+
+      leavitt-app-width-limiter div {
+        background: var(--md-sys-color-surface-container-low);
+        border-radius: 24px;
         padding: 24px;
-        border-radius: 8px;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 12px;
-        margin: 24px 0 36px 0;
+
+        &:not(:first-of-type) {
+          margin-top: 24px;
+        }
+      }
+
+      p {
+        margin-bottom: 12px;
       }
 
       section[buttons] {
@@ -42,37 +56,51 @@ export class TitaniumYoutubeInputDemo extends LitElement {
         align-items: center;
         flex-wrap: wrap;
         gap: 12px;
+        margin-top: 12px;
       }
     `,
   ];
 
   render() {
     return html`
-      <story-header name="Titanium youtube input" className="TitaniumYoutubeInput"></story-header>
-      <h1>Default</h1>
-      <p>YouTube video input with URL validation</p>
-      <div>
-        <titanium-youtube-input label="Video" value="SYmmXDTHx6Q"></titanium-youtube-input>
-      </div>
+      <leavitt-app-main-content-container .pendingStateElement=${this}>
+        <main>
+          <leavitt-app-navigation-header level1Text="Titanium youtube input" level1Href="/titanium-youtube-input" sticky-top> </leavitt-app-navigation-header>
 
-      <h1>Disabled</h1>
-      <p>Disabled YouTube input</p>
-      <div>
-        <titanium-youtube-input disabled label="Video" value="SYmmXDTHx6Q"></titanium-youtube-input>
-      </div>
+          <leavitt-app-width-limiter max-width="1000px">
+            <story-header name="Titanium youtube input" className="TitaniumYoutubeInput"></story-header>
 
-      <h1>Methods</h1>
-      <p>Demonstrates public methods like reset and reportValidity</p>
-      <div>
-        <titanium-youtube-input required label="Video" @input=${(e: DOMEvent<TitaniumYouTubeInput>) => console.log(e.target.value)}></titanium-youtube-input>
-        <br />
-        <section buttons>
-          <md-outlined-button @click=${() => this.requiredInput.reset()}>Reset</md-outlined-button>
-          <md-outlined-button @click=${() => this.requiredInput.reportValidity()}>Report validity</md-outlined-button>
-        </section>
-      </div>
+            <div>
+              <h1>Default</h1>
+              <p>YouTube video input with URL validation</p>
+              <titanium-youtube-input label="Video" value="SYmmXDTHx6Q"></titanium-youtube-input>
+            </div>
 
-      <api-docs src="./custom-elements.json" selected="titanium-youtube-input"></api-docs>
+            <div>
+              <h1>Disabled</h1>
+              <p>Disabled YouTube input</p>
+              <titanium-youtube-input disabled label="Video" value="SYmmXDTHx6Q"></titanium-youtube-input>
+            </div>
+
+            <div>
+              <h1>Methods</h1>
+              <p>Demonstrates public methods like reset and reportValidity</p>
+              <titanium-youtube-input
+                required
+                label="Video"
+                @input=${(e: DOMEvent<TitaniumYouTubeInput>) => console.log(e.target.value)}
+              ></titanium-youtube-input>
+              <br />
+              <section buttons>
+                <md-filled-tonal-button @click=${() => this.requiredInput.reset()}>Reset</md-filled-tonal-button>
+                <md-filled-tonal-button @click=${() => this.requiredInput.reportValidity()}>Report validity</md-filled-tonal-button>
+              </section>
+            </div>
+
+            <api-docs src="./custom-elements.json" selected="titanium-youtube-input"></api-docs>
+          </leavitt-app-width-limiter>
+        </main>
+      </leavitt-app-main-content-container>
     `;
   }
 }
