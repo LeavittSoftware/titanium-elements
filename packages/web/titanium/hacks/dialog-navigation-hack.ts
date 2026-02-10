89b150ec8256897cd5c5d98edbac9fce90cfff70
Kasey Hinton <kaseyjameshinton@gmail.com>
2026-02-10 11:58:39 -0700

    fix(hacks): upgrade hack for maximum flexibility

--- packages/web/titanium/hacks/dialog-navigation-hack.ts
+++ packages/web/titanium/hacks/dialog-navigation-hack.ts
@@ -1,7 +1,7 @@
 import { MdDialog } from '@material/web/dialog/dialog';
 
 /** 
-    Adds a listener to the window popstate event that will close the dialog when the user navigates away from the page.
+    Adds a listener to the navigate event that will close the dialog when the user navigates away from the url the dialog was opened from.
 **/
 
 export function dialogOpenNavigationHack(dialog: MdDialog) {
@@ -11,12 +11,27 @@ export function dialogOpenNavigationHack(dialog: MdDialog) {
     }
   };
   (dialog as any).__closeListener = closeListener;
-  window.addEventListener('popstate', closeListener, false);
+
+  // Persist the path the dialog was opened from so we can compare it to the destination path
+  const fromPath = window.location.pathname;
+  window.navigation.addEventListener('navigate', (navigateEvent) => {
+    const destinationPath = navigateEvent.destination.pathname;
+
+    if (!destinationPath.includes(fromPath)) {
+      navigateEvent.intercept({ handler: closeListener });
+    }
+  });
 }
 
 export function dialogCloseNavigationHack(dialog: MdDialog) {
   if ((dialog as any)?.__closeListener) {
-    window.removeEventListener('popstate', (dialog as any).__closeListener, false);
+    window.removeEventListener('navigate', (dialog as any).__closeListener, false);
     (dialog as any).__closeListener = undefined;
+  }
+}
+
+declare global {
+  interface Window {
+    navigation: any;
   }
 }
